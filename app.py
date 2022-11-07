180304686e3e229f0b6b9d280eabbb4b1c3c733a
Seth Walker <seth.h.walker@gmail.com>
2022-11-07 03:14:20 -0500

    Add README, add GET response

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route("/beacon", methods=['GET', 'POST'])
 def beacon():
+    if (request.method == "GET"):
+        return "<p>Must send data as POST</p>"
+
     payload = request.get_data(as_text=True)
     with open("beacon.log", "a") as f:
         f.write(payload + '\n')
